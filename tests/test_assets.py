2765975ec125da17f3d09a9b743ba8d43997cc5c
yugu <yu.gu@tum.de>
2026-05-14 17:36:41 +0200

    Add AILamp printable adapter files

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -11,6 +11,14 @@ def test_required_assets_and_recordings_are_present():
     assert (root / "simulation/robot.xml").exists()
     assert (root / "simulation/assets/lamp_head.stl").exists()
     assert (root / "3D/LampHead.3mf").exists()
+    adapter_dir = root / "3D/AILamp_Adapters"
+    assert (adapter_dir / "AILamp_Jetson_Nano_Base_Tray.3mf").exists()
+    assert (adapter_dir / "AILamp_Electronics_Side_Deck.3mf").exists()
+    assert (adapter_dir / "AILamp_Head_Camera_Mount.3mf").exists()
+    assert (adapter_dir / "AILamp_NeoMatrix_Holder.3mf").exists()
+    assert (adapter_dir / "AILamp_ReSpeaker_External_Mount.3mf").exists()
+    assert (adapter_dir / "AILamp_Cable_Clip_6mm.3mf").exists()
+    assert (adapter_dir / "AILamp_Cable_Clip_10mm.3mf").exists()
 
     recordings = RecordingStore(root / "ailamp_runtime/ailamp/recordings")
     names = recordings.list_names()
